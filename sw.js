da9e8e4a75a4f600b4a96528cd8c24d5cd9c08b0
lollombardi <lombardilorenzo99@gmail.com>
2026-01-16 14:33:37 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,19 @@
+const CACHE_NAME = 'cf-pro-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './index.png',
+  './manifest.json'
+];
 
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
